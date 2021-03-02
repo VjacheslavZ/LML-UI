b2c71608c236b5cb6c7759f8194a5cef9dfa95ab
VjacheslavZ <vjacheslav.zemlianiy@fluvius.co>
2021-03-02 22:39:14 +0200

    add AuthService, user stroe

---
+++ src/servises/auth.service.ts
@@ -0,0 +1,20 @@
+// @ts-ignore
+import { post } from 'axios';
+import BaseHttpService from './base-http.service';
+
+export default class AuthService extends BaseHttpService {
+  async signin(username: string, password: string) {
+    const result = await post(`${this.BASE_URL}/auth/signin`, { username, password });
+    const accessToken = result.data.accessToken;
+    this.saveToken(accessToken);
+    return result.data.username;
+  }
+
+  async signup(username: string, password: string) {
+    await post(`${this.BASE_URL}/auth/signup`, { username, password });
+  }
+
+  async signout() {
+    this.removeToken();
+  }
+}
